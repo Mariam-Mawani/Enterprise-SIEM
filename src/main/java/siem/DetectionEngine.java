f309fdfbae68d3ce84e3b18a2b209ca9bc1b6b1c
mariam mawani <mariammawani@gmail.com>
2026-09-04 12:01:47 +0200

    created variables for runAllRules

--- src/main/java/siem/DetectionEngine.java
+++ src/main/java/siem/DetectionEngine.java
@@ -6,6 +6,7 @@ package java.siem;
  */
 
 
+import java.util.ArrayList;
 
 public class DetectionEngine {
 
@@ -19,7 +20,16 @@ public class DetectionEngine {
     private static final int PORT_SCAN_PORT_THRESHOLD = 10;     // Number of distinct ports
     private static final long PORT_SCAN_WINDOW_MINUTES = 1;     // Within this time window
 
+    // Public method: run ALL rules and return every alert raised
+    public ArrayList<Alert> runAllRules(ArrayList<LogEvent> events) {
 
+        ArrayList<Alert> allAlerts = new ArrayList<>();
+
+        // Rule each and collect whatever allerts it produces
+        ArrayList<Alert> bruteForceAlerts = checkForBruteForce(events);
+        ArrayList<Alert> portScanAlerts   = checkForPortScan(events);
+
+    }
 
 
 
